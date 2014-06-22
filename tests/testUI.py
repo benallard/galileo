e560ef147a3fd809e8aa2943610e68c287b058e6
Benoît Allard <benoit.allard@gmx.de>
2014-06-22 23:50:38 +0200

    Add a forgotten testsuite

---
+++ tests/testUI.py
@@ -0,0 +1,26 @@
+import unittest
+
+from galileo.ui import Form, FormField
+
+class testFormField(unittest.TestCase):
+    def teststr(self):
+        self.assertEqual("'name': 'value'", str(FormField('name', 'text', 'value')))
+        self.assertEqual("'name': None", str(FormField('name', 'text')))
+
+    def testasXML(self):
+        self.assertEqual(FormField('name').asXMLParam(), ('param', {'name': 'name'}, [], None))
+        self.assertEqual(FormField('name', value='value').asXMLParam(), ('param', {'name': 'name'}, [], 'value'))
+
+class testHTMLForm(unittest.TestCase):
+    def asXML(self):
+        f = Form()
+        f.addField(FormField('name'))
+        f.addField(FormField('name2'))
+        self.assertEqual(f.asXML(), [('param', {'name': 'name'}, [], None), ('param', {'name': 'name2'}, [], None)])
+
+    def asXML2Submit(self):
+        f = Form()
+        f.addField(FormField('name', 'submit'))
+        f.addField(FormField('name2', 'submit'))
+        f.takeValuesFromAnswer({'name2': None})
+        self.assertEqual(f.asXML(), [('param', {'name': 'name2'}, [], None)])
