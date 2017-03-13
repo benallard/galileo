858d0a0f4f75688b184bc0c2a9d59dfdf3b16182
Benoît Allard <benoit.allard@gmx.de>
2017-03-13 12:23:50 +0100

    Closing branch Dybian/improve-galileometainfoxml-to-be-complia-1489354687735

--- galileo/ble/__init__.py
+++ galileo/ble/__init__.py
@@ -1,3 +1,11 @@
 class API(object):
-    def setup():
-       raise NotImplementedError
+    """ This is the general class to subclass in order to implement a bluetooth
+    API """
+    def __init__(self, logSize):
+        """ the logsize parameter indicates how  many of the communication
+        should be displayed """
+        raise NotImplementedError
+
+    def setup(self):
+        """This returns a boolean indicating the success of the operation"""
+        raise NotImplementedError
