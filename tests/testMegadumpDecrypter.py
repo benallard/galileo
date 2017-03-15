f4d60f35431575b56edb0b3a52ffc4d444cb2ea2
HolyGuacamole <spameater21@outlook.com>
2017-03-15 17:16:19 +0100

    Add a test case for the XTEA_CMAC computation

--- tests/testMegadumpDecrypter.py
+++ tests/testMegadumpDecrypter.py
@@ -1,6 +1,6 @@
 import unittest
 
-from galileo.megadumpDecrypter import computeCounter, counter
+from galileo.megadumpDecrypter import computeCounter, counter, XTEA_CMAC
 from galileo.utils import x2a
 
 class TestXTEAMegadumpDecrypter(unittest.TestCase):
@@ -17,6 +17,22 @@ class TestXTEAMegadumpDecrypter(unittest.TestCase):
 
         counter = computeCounter(key, nonce)
         self.assertEqual(counter, bytearray(x2a('a9 3f 69 fc 60 eb 75 25')))
+
+class testXTEA_CMAC(unittest.TestCase):
+
+    def testCmacComputation(self):
+        key = bytearray([
+            0x12, 0x34, 0x56, 0x78,
+            0x12, 0x34, 0x56, 0x78,
+            0x12, 0x34, 0x56, 0x78,
+            0x12, 0x34, 0x56, 0x78
+            ])
+        cmac = XTEA_CMAC(key)
+        cmac.update(x2a('de ad be ef'))
+        result = cmac.final()
+        expectedResult = bytearray(x2a('b5 f3 eb 27 15 45 e5 55'))
+        self.assertEqual(result, expectedResult)
+
 
 class testCounter(unittest.TestCase):
     def testSimple(self):
